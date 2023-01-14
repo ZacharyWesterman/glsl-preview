b554c6f23d0bc4c36b8a25be1c4e90a5f3cac392
Zachary Westerman <westerman.zachary@gmail.com>
2023-01-14 15:41:59 -0600

    remove unneeded cfg dir

--- cfg/default_fragment.glsl
+++
@@ -1,7 +0,0 @@
-uniform float u_time;
-
-void main()
-{
-	float brightness = (sin(u_time) + 2.0) / 5.0;
-	gl_FragColor = vec4(brightness);
-}
--- cfg/vertex.glsl
+++
@@ -1,9 +0,0 @@
-#version 130
-in mediump vec3 point;
-in mediump vec2 texcoord;
-out mediump vec2 UV;
-void main()
-{
-	gl_Position = vec4(point, 1);
-	UV = texcoord;
-}
